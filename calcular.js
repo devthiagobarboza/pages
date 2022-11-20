dd79cf38f95eb70d59cf7131fc09c72d464bd8a9
Thiago Barboza <97811921+devthiagobarboza@users.noreply.github.com>
2022-11-20 19:04:26 -0300

    Mudanças no arquivo CSS.

--- calcular.js
+++ calcular.js
@@ -6,12 +6,22 @@ function calcularMediaPonderada(){
     if (resultadoMediaPonderada >= 6.0){ 
         document.getElementById("resultado").setAttribute("value",resultadoMediaPonderada);
         document.getElementById("txtmedia").setAttribute("value", "Parabéns!! Você foi aprovado!!!");
+        document.getElementById("validaimagem").src = "imagens/aprovado.jpg"; 
+        document.getElementById('resultado').style.color = 'blue';
+        document.getElementById('txtmedia').style.color = 'blue';
+    
     } else if (resultadoMediaPonderada > 3 && resultadoMediaPonderada < 6) {
         document.getElementById("resultado").setAttribute("value",resultadoMediaPonderada);
         document.getElementById("txtmedia").setAttribute("value", "hmmmm... você está em recuperação");
-        document.getElementById("txtchances").setAttribute("value", "Seu objetivo na N2-Sub é obter: " + situacaoDoAluno);
+        document.getElementById("txtchances").setAttribute("value", "SOLUÇÃO: Seu objetivo na N2-Sub é obter: " + situacaoDoAluno);
+        document.getElementById("validaimagem").src = "imagens/recuperacao.jpg";
+        document.getElementById('resultado').style.color = 'yellow';
+        document.getElementById('txtmedia').style.color = 'yellow';
     } else {
         document.getElementById("resultado").setAttribute("value",resultadoMediaPonderada);
         document.getElementById("txtmedia").setAttribute("value", "Lamento! Mas você está reprovado!");
+        document.getElementById("validaimagem").src = "imagens/reprovado.jpg"; 
+        document.getElementById('resultado').style.color = 'red';
+        document.getElementById('txtmedia').style.color = 'red';
     }
 }
